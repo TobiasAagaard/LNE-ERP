30878baf77bd226701e1eae514ec4185c1669493
TobiasAagaard <tach@trifork.com>
2026-05-04 12:24:37 +0200

    Added 3 new test to ProfitPercent

--- ERP-CLI.Tests/ProductTests.cs
+++ ERP-CLI.Tests/ProductTests.cs
@@ -17,4 +17,47 @@ public class ProductTests
 
         Assert.Equal(100, result);
     }
+
+    [Fact]
+
+    public void ProfitPercent_WhenPriceIsEqualToCost_Returns0()
+    {
+        Product product = new Product
+        {
+            Price = 150,
+            Cost = 150,
+        };
+
+        var result = product.ProfitPercent;
+
+        Assert.Equal(0, result);    
+    }
+
+    [Fact]
+    public void ProfitPercent_WhenPriceIsLessThanCost_ReturnsNegative()
+    {
+        Product product = new Product
+        {
+            Price = 80,
+            Cost = 100,
+        };
+
+        var result = product.ProfitPercent;
+
+        Assert.Equal(-20, result);
+    }
+
+    [Fact]
+    public void ProfitPercent_WhenDivisionByZero_ReturnsNull()
+    {
+        Product product = new Product
+        {
+            Price = 100,
+            Cost = 0,
+        };
+
+        var result = product.ProfitPercent;
+
+        Assert.Null(result);
+    }
 }
